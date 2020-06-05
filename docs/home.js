096220840c62b28a0ae7cef2b0a637b55d9a0565
PepperFab <palise128@gmail.com>
2020-06-05 18:44:43 -0400

    Website finished and ready for publication

--- docs/home.js
+++ docs/home.js
@@ -1,4 +1,6 @@
-function welcome() {
-    alert("Welcome to my website where I am presenting my Final Project.")
-};
-welcome();
+var x = false;
+            
+if (x == false) {
+    alert("Welcome to my website where I am presenting my Final Project.");
+    x = true;
+}
